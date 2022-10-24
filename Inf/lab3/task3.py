9c87af40ae8128c22ee3fda29ae6d9163c221dae
ArsenyVekshin <44843792+ArsenyVekshin@users.noreply.github.com>
2022-10-24 09:53:19 +0300

    def - stg2

--- Inf/lab3/task3.py
+++ Inf/lab3/task3.py
@@ -1,17 +1,22 @@
 # isu=367133 --> var=3
 import re
+groupe = "P3116"
+groupe = re.escape(groupe)
+
 
 def main(data):
-    data = re.split(r"[. \-]", data)
-    for i in range(1,len(data)-1):
-        if(len(data[i])==0): continue
-        if(data[i][0]!=data[0][0]): return True
+    data = re.findall(r"[АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ]", data)
+    for i in range(1,len(data)):
+        if(data[i]!=data[0]): return True
     return False
 
 def test():
     f = open("task3_data.txt", encoding="utf-8")
-    for i in range(5):
+    data="   "
+    while(len(data)>0):
         data = f.readline()
+        flag = re.search(groupe, data)
+        if(flag == None): continue
         if(main(data)):
             print(data)
 
