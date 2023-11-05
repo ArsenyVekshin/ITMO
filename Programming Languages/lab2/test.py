b82cefcc6ae55fcfe105c9c4e7c8c00bbc8b61fa
ArsenyVekshin <arsvek87@gmail.com>
2023-11-05 13:26:57 +0300

    PL lab2

---
+++ Programming Languages/lab2/test.py
@@ -0,0 +1,17 @@
+import subprocess
+inp = ["", "first", "kurwaBober", "gtyugytftyuftftftftfngvhgchtfhfhjgfhgfghfghfhgfhgESyrdfuygiygtyugytdrtydertdrtdrtdtrdTRDTRDT$DytftyuftftftftfngvhgchtfhfhjgfhgfghfghfhgfhgfhfhgfhgfgfghFGHFHGFHFHGFHGFHGfghfhgfhgfytfhgfhgfgnfhgfhtfhtgffhfhgfhgfgfghFGHFHGFHFHGFHGFHGfghfhgfhgfytfhgfhgfgnfhgfhtfhtgf"]
+outs = ["", "first word explanation", "", ""]
+errors = ["There is no such word in dictionary", "", "There is no such word in dictionary", "There is no such word in dictionary", "Max word length is 256 symb"]
+test_results = ""
+for i in range(len(inp)):
+    p = subprocess.Popen(["./main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate(input=inp[i].encode())
+    stdout = stdout.decode()
+    stderr = stderr.decode()
+    if stdout == outs[i] and stderr == errors[i]:
+        test_results += "A"
+        print("Test +"+str(i+1)+" passed")
+    else:
+        test_results += "F"
+        print(stdout, stderr, "Expected:",outs[i], errors[i])
+print(test_results)
