162efdc5e3de650565a11d6f889042e6154a7180
ArsenyVekshin <arsvek87@gmail.com>
2024-05-07 12:53:45 +0300

    CompMath lab#5 fix newton convert to pynom

--- CompMath/lab5/main.py
+++ CompMath/lab5/main.py
@@ -12,7 +12,7 @@ def show_plot():
     x_arr = np.linspace(a, b, 1000)
     if(f != None):
         y_arr = [f.calc(x) for x in x_arr]
-        plt.plot(x_arr, y_arr, '--r')
+        plt.plot(x_arr, y_arr, '--r', label=f.tostr())
 
     for p in points:
         plt.scatter(p[0], p[1])
@@ -21,17 +21,19 @@ def show_plot():
     plt.axhline(0, color='black', linewidth=1)
     plt.axvline(0, color='black', linewidth=1)
 
+    plt.legend()
     plt.show()
 
 
 
 
-print(expand_brackets([-1, -2, -3]))
+# print(expand_brackets([-1, -2, -3]))
 
 
 lagrange = Lagrange_Polynomial(points)
-print(lagrange.tostr())
 f = lagrange
+
+print(f.tostr())
 show_plot()
 
 points = [[0.15, 1.25], [0.2, 2.38], [0.33, 3.79], [0.47, 5.44]]
@@ -39,4 +41,5 @@ a, b = 0, 0.5
 newton = Newton_Polynomial(points)
 f = newton
 
+print(f.tostr())
 show_plot()
--- CompMath/lab5/methods/newton.py
+++ CompMath/lab5/methods/newton.py
@@ -22,30 +22,25 @@ class Newton_Polynomial(Polynomial):
             self.tree.append([])
             left, right = 0, depth+1
             for i in range(1, len(self.tree[-2])):
-                print(self.tree[-2][i], self.tree[-2][i-1],"/", points[right][0], points[left][0] )
+                # print(self.tree[-2][i], self.tree[-2][i-1],"/", points[right][0], points[left][0] )
                 self.tree[-1].append((self.tree[-2][i] - self.tree[-2][i-1]) /
                                       (points[right][0] - points[left][0]))
                 right += 1
                 left += 1
 
-        for lvl in self.tree:
-            print(lvl)
 
-        print("-"*8, "\n"*2)
-        # Соберем полином из таблицы
-        # self.koofs[0] = points[0][1]
-        # for i in range(len(points)-1):
-        #     _polynom = [-1 * points[j][0] for j in range(i+1)]
-        #     print(_polynom, self.tree[i][0] )
-        #
-        #     _polynom = expand_brackets(_polynom)
-        #     _polynom = [elem * self.tree[i][0] for elem in _polynom]
-        #
-        #     while len(_polynom)<len(self.koofs): _polynom = [0] + _polynom
-        #     print(len(self.koofs), len(_polynom))
-        #     self.koofs = [self.koofs[i] + _polynom[i] for i in range(len(self.koofs))]
+        self.koofs[0] = points[0][1]
+        for i in range(len(points)-1):
+            _polynom = [-1 * points[j][0] for j in range(i+1)]
 
-    def calc(self, x):
+            _polynom = expand_brackets(_polynom)
+            _polynom = [elem * self.tree[i][0] for elem in _polynom]
+
+            while len(_polynom)<len(self.koofs): _polynom = _polynom + [0]
+            self.koofs = [self.koofs[i] + _polynom[i] for i in range(len(self.koofs))]
+
+
+    def calc_by_tree(self, x):
         out = self.points[0][1]
         for i in range(len(self.points) - 1):
             buff = self.tree[i][0]
@@ -53,9 +48,6 @@ class Newton_Polynomial(Polynomial):
                 buff *= (x - self.points[j][0])
             out+=buff
         return out
-
-
-
 
 
 
